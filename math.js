ec3cb288aea00a4b9893c7e7a1ad7c1d347bd2e4
Shaheed mon <sheheedk07@gmail.com>
2024-11-10 15:13:19 +0530

    commit

--- math.js
+++ math.js
@@ -5,16 +5,19 @@ let x = 3.14;
     let z = 9;
 let   maximum;
 let  minimum;
-
 // x = Math.round(x);
 // x = Math.floor(x);
 // x = Math.ceil(x);
 // x = Math.pow(x, 3);
 //x = Math.sqrt(x);
 // x = Math.abs(x);
+
 // maximum = Math.max(x, y, z);
 // minimum = Math.min(x, y, z);
+
 // x = Math.PI;
-// console.log(maximum)        
-console.log(x)
-// console.log(minimum)
+
+
+       // console.log(maximum)        
+ // console.log(x)
+   // console.log(minimum)
