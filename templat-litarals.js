9ce1473753c9d4b6aed2ff85590b5788b8a34ee7
Shaheed mon <sheheedk07@gmail.com>
2024-12-15 22:13:52 +0530

    commit

--- templat-litarals.js
+++ templat-litarals.js
@@ -1,2 +1,7 @@
- 
- console.log(`my name is ${name}`)
+//  const name=`saidh`
+//  console.log(`my name is ${name}`)
+ function multiply(a, b = 2) {
+    return a * b;
+  }
+  
+  console.log(multiply(5,5));
